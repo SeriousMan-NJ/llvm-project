8fdb12689dba83bdfc1c84305a9f2b989972dd98
Yongwon Shin <ywshin@postech.ac.kr>
2020-07-15 01:07:38 +0900

    harris 벤치마크 통계까지 완전히 뽑음
    - 중복된 부분은 추가로 템플릿으로 추출해야 함

--- llvm/lib/CodeGen/PP2/scripts/benchmark.py
+++ llvm/lib/CodeGen/PP2/scripts/benchmark.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import networkx as nx
 import shutil
 import click
+import csv
+import json
 
 class BenchmarkBase():
     def __init__(self, path):
@@ -26,7 +28,7 @@ class BenchmarkBase():
 
         self.LLC = 'llc'
         self.LLC_PROCESS_FLAGS = '-O3 -regalloc pp2 -pp2-dummy-dump-graph -pp2-dummy-export-graph -pp2-skip'
-        self.LLC_RUN_FLAGS = f'-O3 -filetype=asm -regalloc pp2 -pp2-regalloc {self.BACKEND_REGALLOC}'
+        self.LLC_RUN_FLAGS = f'-O3 -filetype=asm -regalloc pp2 -pp2-regalloc {self.BACKEND_REGALLOC} -stats -stats-json-file'
 
         self.CONDA_S2V_DQN_ENV = 'py27'
 
@@ -34,20 +36,7 @@ class BenchmarkBase():
         os.system(f'rm -rf {self.WORKING_DIR}/tmp')
 
     def compile(self):
-        with open(f'{self.WORKING_DIR}/log.txt', 'w') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'w') as log_err:
-            for filename in os.listdir(self.WORKING_DIR):
-                filename = os.path.join(self.WORKING_DIR, filename)
-                if filename.endswith(".cpp"):
-                    print(f"[COMPILE] {filename}")
-                    process = sp.Popen(f'cd {self.WORKING_DIR} && {self.CXX} {self.CXX_FLAGS} {filename}',
-                        shell=True,
-                        stdout=sp.PIPE,
-                        stderr=sp.PIPE)
-                    stdout, stderr = process.communicate()
-                    if process.returncode != 0:
-                        log_err.write(stderr.decode('utf-8'))
-                        log_err.write(f"Failed to compile: {filename}")
-                        exit(1)
+        raise Exception("Not implemented")
 
     def gen_if_graph(self):
         with open(f'{self.WORKING_DIR}/log.txt', 'a') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'a') as log_err:
@@ -63,7 +52,7 @@ class BenchmarkBase():
                     if process.returncode != 0:
                         log_err.write(stderr.decode('utf-8'))
                         log_err.write(f"Failed to process: {filename}")
-                        exit(2)
+                        exit(301)
 
     def graph_to_pickle(self):
         with open(f'{self.WORKING_DIR}/log.txt', 'a') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'a') as log_err:
@@ -75,7 +64,7 @@ class BenchmarkBase():
             if process.returncode != 0:
                 log_err.write(stderr.decode('utf-8'))
                 log_err.write('Failed to convert IF graphs to pickle')
-                exit(3)
+                exit(401)
 
     def gen_coloring(self):
         print("[COLORING] start coloring")
@@ -136,57 +125,19 @@ class BenchmarkBase():
             if process.returncode != 0:
                 log_err.write(stderr.decode('utf-8'))
                 log_err.write('Failed to color IF graphs')
-                exit(4)
+                exit(501)
 
     def benchmark(self):
-        with open(f'{self.WORKING_DIR}/log.txt', 'a') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'a') as log_err:
-            for i in range(17):
-                print(f'ISEC #{i}:')
-                for filename in os.listdir(self.WORKING_DIR):
-                    filename = os.path.join(self.WORKING_DIR, filename)
-                    if filename.endswith(".ll"):
-                        print(f'[REGALLOC] {filename}')
-                        process = sp.Popen(f'cd {self.WORKING_DIR} && {self.LLC} {self.LLC_RUN_FLAGS} -pp2-isec {i} {filename}',
-                            shell=True,
-                            stdout=sp.PIPE,
-                            stderr=sp.PIPE)
-                        stdout, stderr = process.communicate()
-                        if process.returncode != 0:
-                            log_err.write(stderr.decode('utf-8'))
-                            log_err.write(f"Failed to regalloc: {filename}")
-                            exit(5)
-
-                print(f'[LINKING] start linking')
-                process = sp.Popen(f'cd {self.WORKING_DIR} && {self.LINKER} harrisKernel.s main.s -L{self.HOME}/lib64 -lbenchmark -o harris',
-                    shell=True,
-                    stdout=sp.PIPE,
-                    stderr=sp.PIPE)
-                stdout, stderr = process.communicate()
-                if process.returncode != 0:
-                    log_err.write(stderr.decode('utf-8'))
-                    log_err.write(f"Failed to linking")
-                    exit(6)
-
-                print(f'[BENCHMARK] start benchmarking')
-                process = sp.Popen(f'cd {self.WORKING_DIR} && ./harris --benchmark_out="{self.WORKING_DIR}/result.{i}.txt" --benchmark_out_format=json --benchmark_repetitions=2',
-                    shell=True,
-                    stdout=sp.PIPE,
-                    stderr=sp.PIPE)
-                stdout, stderr = process.communicate()
-                if process.returncode != 0:
-                    log_err.write(stderr.decode('utf-8'))
-                    log_err.write(f"Failed to benchmark")
-                    exit(7)
+        raise Exception("Not implemented")
 
     def gen_statistics(self):
-        # TODO
-        pass
+        raise Exception("Not implemented")
 
     def cleanup(self):
         print("[CLEANUP] start cleanup")
         os.makedirs(f'{self.WORKING_DIR}/tmp', exist_ok=True)
         for filename in os.listdir(self.WORKING_DIR):
-            if len(filename.split('.')) > 3 and filename.split('.')[1] == 'll':
+            if len(filename.split('.')) >= 3 and (filename.split('.')[1] == 'll' or filename.split('.')[-1] == 's' or filename.split('.')[-1] == 'json'):
                 shutil.move(f'{self.WORKING_DIR}/{filename}', f'{self.WORKING_DIR}/tmp/{filename}')
 
     def run(self):
@@ -204,6 +155,140 @@ class BenchmarkHarris(BenchmarkBase):
     def __init__(self, path):
         BenchmarkBase.__init__(self, path)
         self.CXX_FLAGS = f'{self.COMMON_FLAGS} -I{self.HOME}/include -std=c++11 -ffast-math'
+
+    def compile(self):
+        with open(f'{self.WORKING_DIR}/log.txt', 'w') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'w') as log_err:
+            for filename in os.listdir(self.WORKING_DIR):
+                filename = os.path.join(self.WORKING_DIR, filename)
+                if filename.endswith(".cpp"):
+                    print(f"[COMPILE] {filename}")
+                    process = sp.Popen(f'cd {self.WORKING_DIR} && {self.CXX} {self.CXX_FLAGS} {filename}',
+                        shell=True,
+                        stdout=sp.PIPE,
+                        stderr=sp.PIPE)
+                    stdout, stderr = process.communicate()
+                    if process.returncode != 0:
+                        log_err.write(stderr.decode('utf-8'))
+                        log_err.write(f"Failed to compile: {filename}")
+                        exit(201)
+
+    def benchmark(self):
+        with open(f'{self.WORKING_DIR}/log.txt', 'a') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'a') as log_err:
+            for i in range(17):
+                print(f'ISEC #{i}:')
+                for filename in os.listdir(self.WORKING_DIR):
+                    filename = os.path.join(self.WORKING_DIR, filename)
+                    if filename.endswith(".ll"):
+                        print(f'[REGALLOC] {filename}')
+                        process = sp.Popen(f'cd {self.WORKING_DIR} && {self.LLC} {self.LLC_RUN_FLAGS} -pp2-isec {i} {filename}',
+                            shell=True,
+                            stdout=sp.PIPE,
+                            stderr=sp.PIPE)
+                        stdout, stderr = process.communicate()
+                        if process.returncode != 0:
+                            log_err.write(stderr.decode('utf-8'))
+                            log_err.write(f"Failed to regalloc: {filename}")
+                            exit(601)
+
+                print(f'[LINKING] start linking')
+                process = sp.Popen(f'cd {self.WORKING_DIR} && {self.LINKER} harrisKernel.s main.s -L{self.HOME}/lib64 -lbenchmark -o harris',
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+                stdout, stderr = process.communicate()
+                if process.returncode != 0:
+                    log_err.write(stderr.decode('utf-8'))
+                    log_err.write(f"Failed to linking")
+                    exit(602)
+
+                print(f'[BENCHMARK] start benchmarking')
+                process = sp.Popen(f'cd {self.WORKING_DIR} && ./harris --benchmark_out="{self.WORKING_DIR}/result.{i}.json" --benchmark_out_format=json --benchmark_repetitions=2',
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+                stdout, stderr = process.communicate()
+                if process.returncode != 0:
+                    log_err.write(stderr.decode('utf-8'))
+                    log_err.write(f"Failed to benchmark")
+                    exit(603)
+
+                print(f'[PREPARE STATISTICS] prepare statistics')
+                process = sp.Popen(f'mv {self.WORKING_DIR}/stats.json {self.WORKING_DIR}/stats.{i}.json',
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+                stdout, stderr = process.communicate()
+                if process.returncode != 0:
+                    log_err.write(stderr.decode('utf-8'))
+                    log_err.write(f"Failed to prepare statistics")
+                    exit(604)
+
+                for filename in os.listdir(self.WORKING_DIR):
+                    filename = os.path.join(self.WORKING_DIR, filename)
+                    if filename.endswith(".ll"):
+                        process = sp.Popen(f'mv {os.path.splitext(filename)[0]}.s {os.path.splitext(filename)[0]}.{i}.s',
+                            shell=True,
+                            stdout=sp.PIPE,
+                            stderr=sp.PIPE)
+                        stdout, stderr = process.communicate()
+                        if process.returncode != 0:
+                            log_err.write(stderr.decode('utf-8'))
+                            log_err.write(f"Failed to prepare statistics")
+                            exit(605)
+
+    def gen_statistics(self):
+        print(f'[STATISTICS] generate statistics')
+        with open(f'{self.WORKING_DIR}/log.txt', 'a') as log_out, open(f'{self.WORKING_DIR}/err.txt', 'a') as log_err, open(f'{self.WORKING_DIR}/runtime.csv', 'w') as runtime_csv, open(f'{self.WORKING_DIR}/ise.csv', 'w') as ise_csv:
+            # runtime statistics
+            runtime_csv_writer = csv.writer(runtime_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            mean_list = []
+            stddev_list = []
+            for i in range(17):
+                with open(f'{self.WORKING_DIR}/result.{i}.json') as f:
+                    data = json.load(f)
+                    for record in data['benchmarks']:
+                        if record['name'] == 'BENCHMARK_HARRIS/2048/2048_mean':
+                            mean_list.append(record['real_time']/10**6)
+                        elif record['name'] == 'BENCHMARK_HARRIS/2048/2048_stddev':
+                            stddev_list.append(record['real_time']/10**6)
+                        else:
+                            continue
+            runtime_csv_writer.writerow(mean_list + stddev_list)
+
+            # ise statistics
+            ise_csv_writer = csv.writer(ise_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for i in range(17):
+                csv_row = []
+                with open(f'{self.WORKING_DIR}/stats.{i}.json') as f:
+                    data = json.load(f)
+                    csv_row.append(data.get("regalloc.NumMISNodes", None))
+                    csv_row.append(data.get("regalloc.NumSkippedNodes",None))
+                    csv_row.append(data.get("regalloc.NumSplittedOrEvictedVRegsAllocatedByPP2", None))
+                for filename in os.listdir(self.WORKING_DIR):
+                    filename = os.path.join(self.WORKING_DIR, filename)
+                    if filename.endswith(".ll"):
+                        process = sp.Popen(f'cat {os.path.splitext(filename)[0]}.{i}.s | wc -l',
+                            shell=True,
+                            stdout=sp.PIPE,
+                            stderr=sp.PIPE)
+                        stdout, stderr = process.communicate()
+                        if process.returncode != 0:
+                            log_err.write(stderr.decode('utf-8'))
+                            log_err.write(f"Failed to benchmark")
+                            exit(701)
+                        csv_row.append(int(stdout.decode('utf-8')))
+
+                        process = sp.Popen(f'cat {os.path.splitext(filename)[0]}.{i}.s | rg "\-byte Spill" | wc -l',
+                            shell=True,
+                            stdout=sp.PIPE,
+                            stderr=sp.PIPE)
+                        stdout, stderr = process.communicate()
+                        if process.returncode != 0:
+                            log_err.write(stderr.decode('utf-8'))
+                            log_err.write(f"Failed to benchmark")
+                            exit(702)
+                        csv_row.append(int(stdout.decode('utf-8')))
+                ise_csv_writer.writerow(csv_row)
 
 @click.command()
 @click.option('--path', help='benchmark path')
